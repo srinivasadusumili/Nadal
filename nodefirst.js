e096e3904865ea87f435c8032cc5fdd4c04a2e7b
srinivas <srinivasadusumili@gmail.com>
2018-09-03 12:12:40 +0530

    mess

--- nodefirst.js
+++ nodefirst.js
@@ -1,4 +1,4 @@
 	var http=require("http");
     http.createServer(function(request,response){
     response.writeHead(199,{'Content-Type':'text/plain'});
-    response.end('Hello World\n');}).listen(8081);
+    response.end('Hello World\n');}).listen(process.env.PORT || 1345);
